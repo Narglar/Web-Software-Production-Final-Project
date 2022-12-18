3eb36c70e1123bafda150416302e0cc5726d4398
Ilkka Niemi <ilkka.t.niemi@tuni.fi>
2022-12-18 23:16:43 +0200

    Revert "fix test issues"
    This reverts commit 4727fbf7aa12e835da02a3456a4df2830fbfa049

--- backend/src/api/tasks.js
+++ backend/src/api/tasks.js
@@ -22,7 +22,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const { id } = req.params;
     console.log(id);
-    const task = todos.find((t) => t.id === Number(id));
+    const task = tasks.find((t) => t.id === Number(id));
     if (task) {
         res.status(200).json(task);
     } else {
@@ -32,25 +32,25 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     const { id, name } = req.body;
-    todos.push({ id, name });
+    tasks.push({ id, name });
     res.status(201).json({ message: 'Created' });
 });
 
 router.patch('/:id', (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
-    const index = todos.findIndex((t) => t.id === Number(id));
+    const index = tasks.findIndex((t) => t.id === Number(id));
     const updatedTask = {
         id: Number(id),
         name,
     };
-    todos[index] = updatedTask;
+    tasks[index] = updatedTask;
     res.status(200).json({ message: 'Updated' });
 });
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
-    todos = todos.filter((t) => t.id !== Number(id));
+    tasks = tasks.filter((t) => t.id !== Number(id));
     res.status(200).json({ message: 'Deleted' });
 });
 
